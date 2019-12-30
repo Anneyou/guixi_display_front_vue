1c8ed08002a6012e7db3a316623202552f1aabb0
Anneyou <anne_13243188185@163.com>
2019-12-30 16:46:48 +0800

    fix: get response request

--- src/components/Enterprise.vue
+++ src/components/Enterprise.vue
@@ -131,7 +131,11 @@ export default {
     },
 
     fileClickedFunc (fileName) {
-      axios.get(`https://gxzh.cdht.gov.cn/api/v4/forms/${this.information.form_id}/responses`).then(res => {
+      axios.get(`https://gxzh.cdht.gov.cn/api/v4/forms/${this.information.form_id}/responses`, {
+        headers: {
+          Authorization: '7372dee4ff2e6b3876e3b386a336a9171444fba5d3a1e5ae3e23c91d92bb68c6:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lc3BhY2VfaWQiOjF9.WXenxuBIxXEgy_YSmk-PRoElIK7f_gP995N5vdCqSAo'
+        }
+      }).then(res => {
         const response = _.filter(res.data, item => item.id === this.information.response_id)
         const mappedValues = response.mapped_values
         switch (fileName) {
